200df61dc8e4b5498c2fa4d7054ba001e6aeafac
shakar <shakarshahedan@gmail.com>
2021-12-30 09:32:27 +0300

    Updated app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiModule } from './backend/api.module';
 import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
@@ -12,7 +14,9 @@ import { HeroComponent } from './components/hero/hero.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ApiModule.forRoot({ rootUrl: '/api' })
   ],
   providers: [],
   bootstrap: [AppComponent]
